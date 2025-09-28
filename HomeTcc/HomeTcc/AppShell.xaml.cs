809d65c8a07cfae75c6283cd2b21054c9e7c7864
LuishEtec <luishbarreira@yahoo.com>
2025-09-28 20:51:23 -0300

    Projeto adicionado

---
+++ HomeTcc/HomeTcc/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace HomeTcc
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ HomeTcc/HomeTcc/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace HomeTcc
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+       
+    }
+}
