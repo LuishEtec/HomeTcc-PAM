fb8aaf302e44071341b681c61b6ef9da3f709eec
unknown <luishbarreira@yahoo.com>
2025-09-29 07:53:02 -0300

    Alterar informacoes adicionado

--- HomeTcc/HomeTcc/MainPage.xaml.cs
+++ HomeTcc/HomeTcc/MainPage.xaml.cs
@@ -1,13 +1,16 @@
-﻿namespace HomeTcc
-{
-    public partial class MainPage : ContentPage
-    {
-        int count = 0;
+﻿namespace HomeTcc;
 
-        public MainPage()
-        {
-            InitializeComponent();
-        }
-       
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
     }
+    private void OnToggleMenu(object sender, EventArgs e)
+    {
+        MenuSuspenso.IsVisible = !MenuSuspenso.IsVisible;
+        ConteudoPrincipal.IsVisible = !ConteudoPrincipal.IsVisible;
+    }
+
+
 }
